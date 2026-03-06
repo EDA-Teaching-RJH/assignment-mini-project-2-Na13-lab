4270f29dece5f3ccc4e8a5025b4f799c74e6871f
Na13-lab <na13@kent.ac.uk>
2026-03-06 14:02:51 +0000

    welcome the user with a cowsay message

--- utils.py
+++ utils.py
@@ -7,3 +7,7 @@ def validate_student_id(student_id):
     #WHY? To prevent incorrect data entry in the database.
     pattern = r'^STU-/d{5}$'
     return bool(re.match(pattern, student_id))
+def welcome_user(name):
+    #Library Usage: Uses cowsay to welcome the user with a friendly message.
+    #WHY? To improve user experience and make the application more engaging.
+    cowsay.cow(f"Welcome to the Student Management Systen, {name}!")
