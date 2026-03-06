96cc2c86bea9a504292a5f7fbe85c72ed4b1f6aa
Na13-lab <na13@kent.ac.uk>
2026-03-06 14:07:26 +0000

    saving data to json

--- utils.py
+++ utils.py
@@ -12,3 +12,9 @@ def welcome_user(name):
     #Library Usage: Uses cowsay to welcome the user with a friendly message.
     #WHY? To improve user experience and make the application more engaging.
     cowsay.cow(f"Welcome to the Student Management Systen, {name}!")
+
+def save_to_json(data, filename="data.json"):
+    #File I/O and JSON: Saves data
+    #WHY? To prevent data loss and allow for easy data retrieval if needed.
+    with open(filename, 'w') as f:
+        json.dump(data, f, indent=4)
