b92addc3e1dd498c6a3c1f01cd2965bc872f3983
Na13-lab <na13@kent.ac.uk>
2026-03-06 14:00:17 +0000

    validate student ids

--- utils.py
+++ utils.py
@@ -1,3 +1,9 @@
 import re
 import cowsay
 import json
+
+def validate_student_id(student_id):
+    #REGEX:Ensures ID starts with STU- followed by 5 digits.
+    #WHY? To prevent incorrect data entry in the database.
+    pattern = r'^STU-/d{5}$'
+    return bool(re.match(pattern, student_id))
