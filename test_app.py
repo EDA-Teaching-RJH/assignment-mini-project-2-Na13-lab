be8f5cec6814708c775468ad9ab4a16f20a18a09
Na13-lab <na13@kent.ac.uk>
2026-03-06 15:20:37 +0000

    Test if getter works so names are accepted and stored correctly

--- test_app.py
+++ test_app.py
@@ -5,3 +5,11 @@ class TestSystem(unittest.TestCase):
     def test_id_validation(self): #Tests that the REGEX correctly blocks invalid IDs.
         with self.assertRaises(ValueError):
             Student("ErrorUser", "INVALID ID; missing STU prefix")
+    
+    def test_name_validation(self): #Tests if the Getter works. #WHY? To ensure names are being stored.
+        s = Student("Test", "STU-00000")
+        self.assertEqual(s.name, "Test") #Checks that the name is correctly set. 
+        #WHY? To ensure that valid names are being accepted and stored properly.
+        
+if __name__ == "__main__"
+    unittest.main()
