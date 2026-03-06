31924979ec151dc664f807ebeea6fc6aaa73a6a0
Na13-lab <na13@kent.ac.uk>
2026-03-06 15:52:56 +0000

    fixing cowsay by changing different interpreter

--- utils.py
+++ utils.py
@@ -5,13 +5,13 @@ import json
 def validate_student_id(student_id):
     #REGEX:Ensures ID starts with STU- followed by 5 digits.
     #WHY? To prevent incorrect data entry in the database.
-    pattern = r'^STU-/d{5}$'
+    pattern = r'^STU-\d{5}$'
     return bool(re.match(pattern, student_id))
 
 def welcome_user(name):
     #Library Usage: Uses cowsay to welcome the user with a friendly message.
     #WHY? To improve user experience and make the application more engaging.
-    cowsay.cow(f"Welcome to the Student Management Systen, {name}!")
+    cowsay.cow(f"Welcome to the Student Management System, {name}!")
 
 def save_to_json(data, filename="data.json"):
     #File I/O and JSON: Saves data
