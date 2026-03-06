50f103f771910efd71e857a82f95a2cbe276428f
Na13-lab <na13@kent.ac.uk>
2026-03-06 15:16:13 +0000

    Tests if REGEX is blocking invalid IDs

--- test_app.py
+++ test_app.py
@@ -1,2 +1,7 @@
 import unittest
 from app import Student #Imports Student class from app.py to test its functionality.
+
+class TestSystem(unittest.TestCase):
+    def test_id_validation(self): #Tests that the REGEX correctly blocks invalid IDs.
+        with self.assertRaises(ValueError):
+            Student("ErrorUser", "INVALID ID; missing STU prefix")
